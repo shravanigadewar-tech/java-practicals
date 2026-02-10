75c25058eb4b75f26d7e5c5a936f96055d0f50ef
shravanigadewar-tech <shravanigadewar1@gmail.com>
2026-02-10 21:46:02 +0530

    Add files via upload

---
+++ pract6.java
@@ -0,0 +1,12 @@
+class pract6 {
+    public static void main(String[] args) {
+
+        int b = Integer.parseInt("1011", 2);
+        int o = Integer.parseInt("17", 8);
+        int h = Integer.parseInt("1A", 16);
+
+        System.out.println("Binary to Decimal: " + b);
+        System.out.println("Octal to Decimal: " + o);
+        System.out.println("Hex to Decimal: " + h);
+    }
+}
